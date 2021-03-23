c0885fd5c099fa3e43d8e1e239c4c5c00c5adf47
joviannugeraha <joviandmi@gmail.com>
2021-03-23 10:31:12 +0700

    belumsiap

---
+++ bin/latihandart2.dart
@@ -0,0 +1,5 @@
+import 'package:latihandart2/latihandart2.dart' as latihandart2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${latihandart2.calculate()}!');
+}
---
+++ test/latihandart2_test.dart
@@ -0,0 +1,8 @@
+import 'package:latihandart2/latihandart2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
